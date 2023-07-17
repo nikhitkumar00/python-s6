76755d707edc84093f4c4fd23fd19318abbd58d0
nikhitkumar <nikhitkumar00@gmail.com>
2023-07-17 23:54:55 +0530

    updated star

--- star.py
+++ star.py
@@ -1,11 +1,8 @@
 from turtle import Turtle
-import time 
-t = Turtle()
-t.width(4)
-t.color("black","red")
-t.begin_fill()
-for i in range (5):
-    t.forward(300)
+t= Turtle()
+t.up()
+t.goto(-100,100)
+t.down()
+for i in range(5):
+    t.forward(200)
     t.left(144)
-t.end_fill()
-time.sleep(3)
